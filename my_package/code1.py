910c36f6edc6b7f7abc1ef9a8db147000cba7ca2
philmifsud97 <philmifsud97>
2023-11-09 11:25:15 +0000

    tst

--- my_package/code1.py
+++ my_package/code1.py
@@ -1,7 +1,7 @@
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 from pyspark.sql import DataFrame
-
+print("Hello world")
 def generate_data1(spark, n=1000, name='my_cool_data'):
     df = spark.range(0, n)
     df.createOrReplaceTempView(name)
